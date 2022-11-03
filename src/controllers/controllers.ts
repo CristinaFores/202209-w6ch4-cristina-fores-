091c49fe431a97e8ef4009ababad71f1d0e439fa
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-04 00:38:03 +0100

    Add endpoint getThingsId

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -7,3 +7,18 @@ export const getThings = (req: Request, res: Response) => {
   console.log(chalk.bgMagenta("OOOH"));
   res.status(200).json({ thingsKnows });
 };
+
+export const getThingsId = (req: Request, res: Response) => {
+  const { idThing } = req.params;
+
+  const thingKnowId = thingsKnows.find(
+    (thingKnow) => thingKnow.id === Number(idThing)
+  );
+
+  if (!thingKnowId) {
+    res.status(404).json({ error: "Sorry id not found 🙅‍♀️ " });
+    return;
+  }
+
+  res.status(200).json({ thingKnowId });
+};
